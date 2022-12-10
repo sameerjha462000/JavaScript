b176a39a5f735a1a87cce64ba71f6149ad9a54f7
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-12-10 18:26:54 +0530

    ㅤ

---
+++ 14. Twisting the DOM to our will/144. Altering styles.js
@@ -0,0 +1,11 @@
+const h1 = document.querySelector("h1");
+
+// We can use the style property to change the current properties but we cannot use it to read
+h1.style; // gives us a CSSStyleDeclaration
+
+
+
+// All of the properties in javascript are camelCased
+
+// In actual we have backgorund-color property
+p.style.backgroundColor = "teal";
