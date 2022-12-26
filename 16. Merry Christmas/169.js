12b8c63fcc0c242e20278cd31c072ad1a53d1ca6
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-12-26 10:32:05 +0530

    ㅤ

--- 16. Merry Christmas/169.js
+++ 16. Merry Christmas/169.js
@@ -7,18 +7,31 @@ const fakeRequest = (url) => {
             if (rand < 0.3) {
                 reject({status : 404});
             } else {
-                resolve();
+                const pages = {
+                    '/users': [
+                        { id: 1, username: 'Bilbo' },
+                        { id: 5, username: 'Esmerelda' }
+                    ],
+                    '/about': 'This is the about page!'
+                };
+
+                const data = pages[url];
+
+                // This object would be passed inside .then() method as an argument
+                resolve({ status: 200, data });
+                
             }
         }, 3000);
     });
 }
 
 
-fakeRequest()
-    .then(() => { 
+fakeRequest('/users')
+    .then((res) => { 
+        console.log(res.data);
         console.log('REQUEST WORKED!');
     })
-    .catch((err) => { 
-        console.log(err.status);
+    .catch((res) => { 
+        console.log(res.status);
         console.log('REQUEST FAILED!');
     })
